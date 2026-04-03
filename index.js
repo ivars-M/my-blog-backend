fc82da308a7236861b198eedc575faa33b0a8932
ivars-M <ivars.muizarajs@gmail.com>
2026-04-03 16:01:22 +0300

    Fix CORS origin and DB user spelling

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://ideja-prakse-pieredze.vercel.app/",
+      "https://ideja-prakse-pieredze.vercel.app",
     ],
     credentials: true,
   }),
